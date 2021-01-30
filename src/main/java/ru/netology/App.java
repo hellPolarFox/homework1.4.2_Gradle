c886e1af6789fe1ffae375674fee0f71188a1359
Ildar Akhmetkhanov <akhmetkhanovis@gmail.com>
2021-01-30 21:39:15 +0500

    Initial commit

---
+++ src/main/java/ru/netology/App.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+/**
+ * App.
+ *
+ * @author Ilya_Sukhachev
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
